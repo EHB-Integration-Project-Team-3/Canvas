3f6c9bfaca72041c5ac22bb461790f6a845ba8e8
Sven <sve313@gmail.com>
2021-06-03 16:12:12 +0200

    added attendance bugfixing create/update/delete all branches

---
+++ CanvasRabbitMQReceiver/Attendance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CanvasRabbitMQReceiver
+{
+    [XmlRoot(ElementName = "Attendance")]
+    public class Attendance
+    {
+
+        public Attendance() {}
+
+        public Attendance(AttendanceHeader attendanceHeader, String uuid, String creatorId, String attendeeId, String eventId)
+        {
+            AttendanceHeader = attendanceHeader;
+            Uuid = uuid;
+            CreatorId = creatorId;
+            AttendeeId = attendeeId;
+            EventId = eventId;
+        }
+
+            [XmlElement("header")]
+            public AttendanceHeader AttendanceHeader { get; set; }
+
+            [XmlElement("uuid")]
+            public string Uuid { get; set; }
+
+            [XmlElement("creatorId")]
+            public string CreatorId { get; set; }
+
+            [XmlElement("attendeeId")]
+            public string AttendeeId { get; set; }
+
+            [XmlElement("eventId")]
+            public string EventId { get; set; }
+        
+}
+    
+    
+    [Serializable]
+    public class AttendanceHeader {
+        
+        [XmlElement("method")]
+        public string Methode { get; set;}
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using MySql.Data.MySqlClient;
+using Npgsql;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string constring1 = "Server=10.3.17.63,3306;Database=masteruuid;User ID = muuid; Password = muuid;";
+            string constring2 = "Server=10.3.17.64,3306;Database=masteruuid;User ID = muuid; Password = muuid;";
+            string sql = "INSERT INTO master (UUID,Source_EntityId,EntityType,Source " +
+                "VALUES(UUID_TO_BIN(@uuid),@source_entityid,@entitytype,@source); ";
+
+            String source = "CANVAS";
+            String uuid = "test";
+            String localId = "test";
+            String type = "TEST";
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(constring1))
+                {
+                    MySqlCommand command = new MySqlCommand(sql, connection);
+                    connection.Open();
+                    /*
+                    command.Parameters.Add("@uuid", MySqlDbType.VarChar);
+                    command.Parameters.Add("@source_entityid", MySqlDbType.VarChar);
+                    command.Parameters.Add("@entitytype", MySqlDbType.VarChar);
+                    command.Parameters.Add("@source", MySqlDbType.VarChar);
+
+                    command.Parameters["@uuid"].Value = uuid;
+                    command.Parameters["@source_entityid"].Value = localId;
+                    command.Parameters["@entitytype"].Value = type;
+                    command.Parameters["@source"].Value = source;*/
+                    command.Parameters.AddWithValue("@uuid",uuid);
+                    command.Parameters.AddWithValue("@source_entityid",localId);
+                    command.Parameters.AddWithValue("@entitytype",type);
+                    command.Parameters.AddWithValue("@source",source);
+
+                    command.Prepare();
+
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine("Failed to connect to first server for muuid");
+                Console.WriteLine(a.ToString());
+                try
+                {
+                    using (MySqlConnection connection = new MySqlConnection(constring2))
+                    {
+                        MySqlCommand command = new MySqlCommand(sql, connection);
+                        connection.Open();
+                        command.Parameters.Add("@uuid", MySqlDbType.VarChar);
+                        command.Parameters.Add("@source_entityid", MySqlDbType.VarChar);
+                        command.Parameters.Add("@entitytype", MySqlDbType.VarChar);
+                        command.Parameters.Add("@source", MySqlDbType.VarChar);
+
+                        command.Parameters["@uuid"].Value = uuid;
+                        command.Parameters["@source_entityid"].Value = localId;
+                        command.Parameters["@entitytype"].Value = type;
+                        command.Parameters["@source"].Value = source;
+                        command.Prepare();
+
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                        connection.Close();
+                    }
+                }
+                catch (Exception b)
+                {
+                    Console.WriteLine("Failed to connect to second server for muuid");
+                    Console.WriteLine(b.ToString());
+                    throw;
+                }
+                throw;
+            }
+        }
+    }
+}
